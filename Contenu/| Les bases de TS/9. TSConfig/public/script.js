2fc875f577a11b5e8b062496db1cab6f0aff05fa
e-mandy <andymfrd02@gmail.com>
2025-11-01 20:12:42 +0100

    Partie cours - Comprendre le fichier tsconfig

---
+++ Contenu/| Les bases de TS/9. TSConfig/public/script.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const arr = [1, 2, 3];
+const newArr = [...arr, 4, 5, 6];
+console.log(newArr);
+const pNodeList = document.querySelectorAll('p');
+const arrFromNodeList = [...pNodeList];
+//# sourceMappingURL=script.js.map
