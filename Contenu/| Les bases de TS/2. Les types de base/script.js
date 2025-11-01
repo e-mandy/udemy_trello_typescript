593668334b3f0dd318c2f224eb25a334200cd5e3
e-mandy <andymfrd02@gmail.com>
2025-11-01 13:53:07 +0100

    Partie cours - Les types en typescript

---
+++ Contenu/| Les bases de TS/2. Les types de base/script.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let str = "str";
+let num = 5;
+let array = [];
+let obj = {
+    a: 5
+};
+let toggle = true;
+let anything;
+let randomNUmber;
+const conversion = (celsius) => {
+    return (celsius * 9 / 5) + 32;
+};
+console.log(conversion(10));
+//# sourceMappingURL=script.js.map
