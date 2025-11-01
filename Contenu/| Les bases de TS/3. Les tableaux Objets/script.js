70b3e3326ab96647a5647b2318ac98830eecaa8e
e-mandy <andymfrd02@gmail.com>
2025-11-01 14:43:09 +0100

    Partie cours - Les objets et les tableaux

---
+++ Contenu/| Les bases de TS/3. Les tableaux Objets/script.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fruits = ['mangue', 'pomme'];
+fruits.push('cerises');
+console.log(fruits);
+const mixedArray = [1, 'txt', [1, 2, 3]];
+let nums;
+const car = {
+    name: "Audi",
+    model: "A1",
+    km: 70000
+};
+let profile;
+//# sourceMappingURL=script.js.map
---
+++ Contenu/| Les bases de TS/3. Les tableaux Objets/script.ts
@@ -0,0 +1,21 @@
+const fruits = ['mangue', 'pomme'];
+fruits.push('cerises')
+console.log(fruits)
+
+const mixedArray = [1, 'txt', [1, 2, 3]]
+
+
+let nums: number[];
+
+const car = {
+    name: "Audi",
+    model: "A1",
+    km: 70000
+}
+
+let profile : {
+    name: string,
+    age: number,
+    hobbies: string[]
+}
+
