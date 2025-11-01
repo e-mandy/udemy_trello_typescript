caa3776b54e25cb01813328dd14f51f36b62b652
e-mandy <andymfrd02@gmail.com>
2025-11-01 23:54:17 +0100

    Ajout de commentaires pour une meilleure compréhension

--- Projets/scripts/app.js
+++ Projets/scripts/app.js
@@ -3,15 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //  Déjà on récupère tous les conteneurs d'items de notre app (ILs seront enregistrés comme une NodeList d'élément HTML)
 const itemsContainer = document.querySelectorAll('.items-container');
 let actualContainer, actualBtn, actualUL, actualForm, actualTextInput, actualValidation;
+// Ici on s'assure d'ajouter à nos conteneurs, tous les listeners qu'il faut pour toutes les actions qu"il y aura à éffectuer
+// Du coup on récupère un container et on lui ajoute les listeners
 function addContainerListener(currentContainer) {
+    // On récupère le bouton de suppression de Container
     const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn');
+    // On récupère le bouton ajout d'item (l'ajout ne se fait pas encore, il ne fait qu'ouvrir le form)
     const currentAddItemBtn = currentContainer.querySelector('.add-item-btn');
+    //Ici, sont appelés les fonctions qui se chargeront de faire les ajouts de listeners spécifiques
+    // Le listener sur le delete de container. Il récupère le bonton et lui applique le listener
     deleteBtnListeners(currentContainerDeletionBtn);
     addItemBtnListeners(currentAddItemBtn);
 }
+// La boucle qui enclenche la fonction pour l'ajout des listeners aux containers.
 itemsContainer.forEach((container) => {
     addContainerListener(container);
 });
+// La fonction qui récupère le bouton et lui affecte le listener click pour la suppression de container
 function deleteBtnListeners(btn) {
     btn.addEventListener('click', handleContainerDeletion);
 }
--- Projets/src/app.ts
+++ Projets/src/app.ts
@@ -9,25 +9,39 @@ let actualContainer: HTMLDivElement,
     actualValidation: HTMLSpanElement
 
 
+// Ici on s'assure d'ajouter à nos conteneurs, tous les listeners qu'il faut pour toutes les actions qu"il y aura à éffectuer
+// Du coup on récupère un container et on lui ajoute les listeners
 function addContainerListener(currentContainer: HTMLDivElement){
 
+    // On récupère le bouton de suppression de Container
     const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
+
+    // On récupère le bouton ajout d'item (l'ajout ne se fait pas encore, il ne fait qu'ouvrir le form)
     const currentAddItemBtn = currentContainer.querySelector('.add-item-btn') as HTMLButtonElement;
 
+    //Ici, sont appelés les fonctions qui se chargeront de faire les ajouts de listeners spécifiques
 
+    // Le listener sur le delete de container. Il récupère le bonton et lui applique le listener
     deleteBtnListeners(currentContainerDeletionBtn)
+
+    
     addItemBtnListeners(currentAddItemBtn)
 }
 
+// La boucle qui enclenche la fonction pour l'ajout des listeners aux containers.
 itemsContainer.forEach((container: HTMLDivElement) => {
     addContainerListener(container)
 })
 
+// La fonction qui récupère le bouton et lui affecte le listener click pour la suppression de container
 function deleteBtnListeners(btn: HTMLButtonElement){
+    // Au click il appelle la fonction handleContainerDeletion pour la suppression du container
     btn.addEventListener('click', handleContainerDeletion)
 }
  
+// La fonction qui récupère le bouton d'ajout d'item 
 function addItemBtnListeners(btn: HTMLButtonElement){
+
     btn.addEventListener('click', handleAddItem)
 }
 
